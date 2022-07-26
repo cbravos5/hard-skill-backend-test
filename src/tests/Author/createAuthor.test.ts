99da130997ba55871f207ef967c0a43167c8d3c6
cbravos5 <cnbravos5@gmail.com>
2022-07-26 20:09:24 -0300

    test: add update Author test

--- src/tests/Author/createAuthor.test.ts
+++ src/tests/Author/createAuthor.test.ts
@@ -1,4 +1,5 @@
 import app from "@/app";
+import Author from "@/models/Author";
 import supertest from "supertest";
 import { dbTestFunctions } from "../db";
 
@@ -12,7 +13,7 @@ afterAll(async () => await dbTestFunctions.closeDatabase());
 
 afterEach(async () => await dbTestFunctions.clearDatabase());
 
-test("create and Author and retirns it", async () => {
+test("create an Author and returns it", async () => {
   const authorData = {
     firstName: "John",
     lastName: "Doe",
@@ -24,5 +25,33 @@ test("create and Author and retirns it", async () => {
     .post("/authors/create")
     .set({ "Content-Type": "application/json" })
     .send(authorData);
+
   expect(res.body.author.firstName).toEqual(authorData.firstName);
+
+  const author = await Author.findOne({ id: res.body.author._id });
+
+  expect(author).toBeTruthy();
+});
+
+test("update an Author and returns it", async () => {
+  const authorData = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    email: "john.doe@email.com",
+  };
+
+  const oldAuthor = await Author.create(authorData);
+  const newEmail = "john@email.com";
+
+  const res = await request
+    .patch(`/authors/update/${oldAuthor._id}`)
+    .set({ "Content-Type": "application/json" })
+    .send({ email: newEmail });
+
+  expect(res.body.author.email).toEqual(newEmail);
+
+  const author = await Author.findOne({ id: oldAuthor._id });
+
+  expect(author.email).toEqual(newEmail);
 });
