45625fe4c62cb617746248ce731c1dee58a57a73
cbravos5 <cnbravos5@gmail.com>
2022-07-26 19:17:05 -0300

    feat: add test db connection

---
+++ src/tests/db.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import app from "../app";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { Server } from "http";
+
+const dbTest = new MongoMemoryServer();
+let server: Server;
+
+const connect = async () => {
+  await dbTest.start();
+  const dbURI = dbTest.getUri();
+
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: true,
+    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  };
+
+  await mongoose.connect(dbURI, options);
+
+  server = app.listen(3000, () => {
+    console.log(`server listening on 3000`);
+  });
+};
+
+const closeDatabase = async () => {
+  server.close();
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await dbTest.stop();
+};
+
+const clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) await collections[key].drop();
+};
+
+export const dbTestFunctions = { connect, closeDatabase, clearDatabase };
