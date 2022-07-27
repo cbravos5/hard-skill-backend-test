fc5efe5391052f595528e7c0c9507cfa84d7a57d
cbravos5 <cnbravos5@gmail.com>
2022-07-27 10:37:00 -0300

    feat: update comments operations for article ref

--- src/controllers/CommentController.ts
+++ src/controllers/CommentController.ts
@@ -1,3 +1,4 @@
+import Article from "@/models/Article";
 import Author from "@/models/Author";
 import Comment, { IComment } from "@models/Comment";
 import { Request, Response } from "express";
@@ -5,10 +6,18 @@ import { StatusCodes } from "http-status-codes";
 
 const createComment = async (req: Request, res: Response) => {
   try {
-    const { text, creator } = req.body;
+    const { text, creator, articleId } = req.body;
+
+    const article = await Article.findById(articleId);
+
+    if (!article)
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Article not found" });
 
     const comment = new Comment({
       text,
+      article,
       creator: creator || null,
     });
 
@@ -24,7 +33,7 @@ const readComment = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
 
-    const comment = await Comment.findById(id);
+    const comment = await Comment.findById(id).populate("article");
 
     if (!comment)
       return res
@@ -58,7 +67,7 @@ const updateComment = async (req: Request, res: Response) => {
         .status(StatusCodes.NOT_FOUND)
         .json({ message: "Comment not found" });
 
-    const { text, creator } = req.body;
+    const { text } = req.body;
 
     comment.set({
       text: text || comment.text,
--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -1,8 +1,10 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { IAuthor } from "./Author";
+import { IArticle } from "./Article";
 
 export interface IComment {
   text: string;
+  article: IArticle;
+  creator?: string;
 }
 
 export interface ICommentModel extends IComment, Document {}
@@ -10,6 +12,11 @@ export interface ICommentModel extends IComment, Document {}
 const CommentSchema: Schema = new Schema(
   {
     text: { type: "String", required: true },
+    article: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Article",
+      required: true,
+    },
     creator: { type: "String" },
   },
   {
